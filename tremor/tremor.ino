61e3f41e416446e4f22af6e852f4e1d3fd242221
MKellish <mark.kellish@gmail.com>
2022-02-26 04:59:40 -0500

    improved readability and comments

--- tremor/tremor.ino
+++ tremor/tremor.ino
@@ -1,7 +1,10 @@
+// Adjust these vars to change tremor characteristics
 double magnitude = 0.1; // in mm
 double freq = 1; // in Hz
 int runTime = 1; // in minutes
 double probeDist = 103.95; // in mm distance center of hinge to measurement location
+
+// Define pins according to wiring hook ups
 #define STP 9 //step pin
 #define DIR 8 //direction pin
 #define MS1 12 //microstep 1 pin
@@ -9,69 +12,86 @@ double probeDist = 103.95; // in mm distance center of hinge to measurement loca
 #define MS3 10 //microstep 3 pin
 #define EN 13 //enable pin
 #define STSW 6 //switch position indicator
+
+// Initialize variables
 int amplitude;
 int delayTime;
 int startSwitchState =0;
 int x;
 
 void setup() {
-// put your setup code here, to run once:
-Serial.begin(9600); //Open Serial connection for debugging
-pinMode(STP, OUTPUT);
-pinMode(DIR, OUTPUT);
-pinMode(MS1, OUTPUT);
-pinMode(MS2, OUTPUT);
-pinMode(MS3, OUTPUT);
-pinMode(EN, OUTPUT);
-pinMode(STSW, INPUT);
-amplitude = ((magnitude/probeDist)*132.84)/0.0254*16*1.55; //calculate number of steps to reach desired mangitude
-delayTime = (freq/2)/amplitude*1000; //calculate delay time in microseconds between steps to reach desired frequency
-// enables motor movement at full step mode
-digitalWrite(EN, LOW);
-digitalWrite(MS1, LOW);
-digitalWrite(MS2, LOW);
-digitalWrite(MS3, LOW);
-// brings motor to minimum extension position
-digitalWrite(DIR, HIGH); for(x= 1; x<520; x++) //Loop the forward stepping enough times for motion to be visible
-{
-digitalWrite(STP,HIGH); //Trigger one step forward
-delay(1);
-digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
-delay(1);
+  
+  Serial.begin(9600); //Open Serial connection for debugging
+  
+  pinMode(STP, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(MS3, OUTPUT);
+  pinMode(EN, OUTPUT);
+  pinMode(STSW, INPUT);
+  
+  amplitude = ((magnitude/probeDist)*132.84)/0.0254*16*1.55; //calculate number of steps to reach desired mangitude
+  delayTime = (freq/2)/amplitude*1000; //calculate delay time in microseconds between steps to reach desired frequency
+  
+  // enables motor movement at full step mode
+  digitalWrite(EN, LOW);
+  digitalWrite(MS1, LOW);
+  digitalWrite(MS2, LOW);
+  digitalWrite(MS3, LOW);
+  
+  // brings motor to minimum extension position
+  digitalWrite(DIR, HIGH);
+  
+  for(x= 1; x<520; x++) //Loop the forward stepping enough times for motion to be visible
+    {
+      digitalWrite(STP,HIGH); //Trigger one step forward
+      delay(1);
+      digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
+      delay(1);
+    }
+  
+  // positions motor to hold arm parallel to mounting plate
+  digitalWrite(DIR, LOW);
+  
+  for(x= 1; x<251; x++) //Loop the forward stepping enough times for motion to be visible
+    {
+      digitalWrite(STP,HIGH); //Trigger one step forward
+      delay(1);
+      digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
+      delay(1);
+    }
+  
+  // sets to 1/16 microstep mode
+  digitalWrite(MS1, HIGH);
+  digitalWrite(MS2, HIGH);
+  digitalWrite(MS3, HIGH);
 }
-// positions motor to hold arm parallel to mounting plate
-digitalWrite(DIR, LOW);
-for(x= 1; x<251; x++) //Loop the forward stepping enough times for motion to be visible
-{
-digitalWrite(STP,HIGH); //Trigger one step forward
-delay(1);
-digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
-delay(1);
-}
-// sets to 1/16 microstep mode
-digitalWrite(MS1, HIGH);
-digitalWrite(MS2, HIGH);
-digitalWrite(MS3, HIGH);
-}
-void loop() {
-// put your main code here, to run repeatedly:
-startSwitchState = digitalRead(STSW);
-while (true==true)
-{
-  digitalWrite(DIR, LOW); //Pull direction pin low to move "forward"
-for (int x=0;x<amplitude;x++) //move forward until desired magnitude is reached
-{
-digitalWrite(STP,HIGH); //Trigger one step forward
-digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
-delay(delayTime); //wait calculated delay between steps
-}
-digitalWrite(DIR, HIGH); //Pull direction pin high to move "reverse"
-for (int x=0;x<amplitude;x++) //move backward until return to "home" position
-{
-digitalWrite(STP,HIGH); //Trigger one step forward
-digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
-delay(delayTime); //wait calculated delay between steps
-}
-startSwitchState = digitalRead(STSW); //check switch status
-}
+
+void loop(){
+  
+  startSwitchState = digitalRead(STSW);
+  
+  while (true==true)
+    {
+      digitalWrite(DIR, LOW); //Pull direction pin low to move "forward"
+      
+      for (int x=0;x<amplitude;x++) //move forward until desired magnitude is reached
+        {
+          digitalWrite(STP,HIGH); //Trigger one step forward
+          digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
+          delay(delayTime); //wait calculated delay between steps
+        }
+  
+      digitalWrite(DIR, HIGH); //Pull direction pin high to move "reverse"
+      
+      for (int x=0;x<amplitude;x++) //move backward until return to "home" position
+        {
+          digitalWrite(STP,HIGH); //Trigger one step forward
+          digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
+          delay(delayTime); //wait calculated delay between steps
+        }
+  
+      startSwitchState = digitalRead(STSW); //check switch status
+    }
 }
--- twitch/twitch.ino
+++ twitch/twitch.ino
@@ -1,8 +1,10 @@
+// Adjust these vars to change twitch characteristics
 double magnitude = 1; // in mm
 double avgvelocity = 10 ; //in mm/s
 double interval = 30; // in s time between twitch
 int runTime = 1; // in minutes
-//Declare pin functions on Arduino
+
+//Define pins according to wiring hook ups
 #define STP 4 //step pin for motor 1
 #define DIR 3 //direction pin for motor 1
 #define EN 2 //enable pin for motor 1
@@ -10,62 +12,80 @@ int runTime = 1; // in minutes
 #define DIR2 8 //direction pin for motor 2
 #define EN2 6 //enable pin for motor 2
 #define STSW 12 //switch position indicator
+
+// Initialize variables
 int amplitude;
 int startSwitchState =0;
 int x;
 int delayTime;
 int delayInterval;
+
 void setup() {
-Serial.begin(9600); //Open Serial connection for debugging
-pinMode(STP, OUTPUT);
-pinMode(DIR, OUTPUT);
-pinMode(EN, OUTPUT);
-pinMode(STSW, INPUT);
-pinMode(STP2, OUTPUT);
-pinMode(DIR2, OUTPUT);
-pinMode(EN2, OUTPUT);
-pinMode(STSW, INPUT);
-digitalWrite(EN, LOW); //enables movement of motor 1
-digitalWrite(EN2, LOW); //enables movement of motor 2
-amplitude = (magnitude/0.2)*16; //calculate number of steps required to reach magnitude
-delayTime = (magnitude/avgvelocity)*1000000/amplitude-40; //calculate delay time in microseconds required to attain avg velocity
-delayInterval = interval*1000; //convert input interval to milliseconds
+  
+  Serial.begin(9600); //Open Serial connection for debugging
+  
+  pinMode(STP, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(EN, OUTPUT);
+  pinMode(STSW, INPUT);
+  pinMode(STP2, OUTPUT);
+  pinMode(DIR2, OUTPUT);
+  pinMode(EN2, OUTPUT);
+  pinMode(STSW, INPUT);
+  
+  digitalWrite(EN, LOW); //enables movement of motor 1
+  digitalWrite(EN2, LOW); //enables movement of motor 2
+  
+  amplitude = (magnitude/0.2)*16; //calculate number of steps required to reach magnitude
+  delayTime = (magnitude/avgvelocity)*1000000/amplitude-40; //calculate delay time in microseconds required to attain avg velocity
+  delayInterval = interval*1000; //convert input interval to milliseconds
 }
+
 void loop() {
+
 startSwitchState = digitalRead(STSW);
+
 while (STSW==HIGH) //occurs only while switch is turned on
-{
-digitalWrite(DIR, HIGH); //Pull direction pin high to move "forward"
-for (int x=0;x<amplitude;x++) //move forward until desired magnitude is reached
-{
-digitalWrite(STP,HIGH); //Trigger one step forward
-digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
-delayMicroseconds(delayTime); //wait calculated delay between steps
-}
-digitalWrite(DIR, LOW); //Pull direction pin low to move "reverse"
-for (int x=0;x<amplitude;x++) //move backward until return to "home" position
-{
-digitalWrite(STP,HIGH); //Trigger one step forward
-digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
-delayMicroseconds(delayTime); //wait calculated delay between steps
-}
-startSwitchState = digitalRead(STSW); //check switch status
-delay(delayInterval); //wait calculated time between twitches
-digitalWrite(DIR2, HIGH); //Pull direction pin high to move "forward"
-for (int x=0;x<amplitude;x++) //move forward until desired magnitude is reached
-{
-digitalWrite(STP2,HIGH); //Trigger one step forward
-digitalWrite(STP2,LOW); //Pull step pin low so it can be triggered again
-delayMicroseconds(delayTime); //wait calculated delay between steps
-}
-digitalWrite(DIR2, LOW); //Pull direction pin low to move "reverse"
-for (int x=0;x<amplitude;x++) //move backward to return to "home" position
-{
-digitalWrite(STP2,HIGH); //Trigger one step forward
-digitalWrite(STP2,LOW); //Pull step pin low so it can be triggered again
-delayMicroseconds(delayTime); //wait calculated delay between steps
-}
-startSwitchState = digitalRead(STSW); //check swtich status
-delay(delayInterval); //wait calculated time between twitches
-}
+  {
+    digitalWrite(DIR, HIGH); //Pull direction pin high to move "forward"
+    
+    for (int x=0;x<amplitude;x++) //move forward until desired magnitude is reached
+      {
+        digitalWrite(STP,HIGH); //Trigger one step forward
+        digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
+        delayMicroseconds(delayTime); //wait calculated delay between steps
+      }
+      
+    digitalWrite(DIR, LOW); //Pull direction pin low to move "reverse"
+    
+    for (int x=0;x<amplitude;x++) //move backward until return to "home" position
+      {
+        digitalWrite(STP,HIGH); //Trigger one step forward
+        digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
+        delayMicroseconds(delayTime); //wait calculated delay between steps
+      }
+    
+    startSwitchState = digitalRead(STSW); //check switch status
+    delay(delayInterval); //wait calculated time between twitches
+    digitalWrite(DIR2, HIGH); //Pull direction pin high to move "forward"
+    
+    for (int x=0;x<amplitude;x++) //move forward until desired magnitude is reached
+      {
+        digitalWrite(STP2,HIGH); //Trigger one step forward
+        digitalWrite(STP2,LOW); //Pull step pin low so it can be triggered again
+        delayMicroseconds(delayTime); //wait calculated delay between steps
+      }
+    
+    digitalWrite(DIR2, LOW); //Pull direction pin low to move "reverse"
+    
+    for (int x=0;x<amplitude;x++) //move backward to return to "home" position
+      {
+        digitalWrite(STP2,HIGH); //Trigger one step forward
+        digitalWrite(STP2,LOW); //Pull step pin low so it can be triggered again
+        delayMicroseconds(delayTime); //wait calculated delay between steps
+      }
+    
+    startSwitchState = digitalRead(STSW); //check swtich status
+    delay(delayInterval); //wait calculated time between twitches
+  }
 }
